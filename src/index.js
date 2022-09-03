693448a79b43891568c75efa25dc982be3ef8865
Oscar Bucio <oscarbucio2001@gmail.com>
2022-09-03 14:29:03 -0500

    Hello world

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// Se importan las librerias necesarias, están instaladas en el proyecto y se muestran en el package.json
+
+// Se monta la aplicación
+
+// Referencia del div root
+// Forma larga
+const rootElement = document.getElementById('root');
+ReactDOM.createRoot(rootElement); // Se crea el root
+
+// Forma corta
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Renderizar en la pagina
+root.render(<h1>Hola mundo desde React!</h1>);
+
+
+// Componentes en React
