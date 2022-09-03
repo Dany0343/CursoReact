f670eab2de31136cd1fa8e2e249ba4c42b72a6b0
Oscar Bucio <oscarbucio2001@gmail.com>
2022-09-03 17:13:45 -0500

    jsx done

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
 // Se importan las librerias necesarias, están instaladas en el proyecto y se muestran en el package.json
 
 // Se monta la aplicación
 
 // Referencia del div root
-// Forma larga
-const rootElement = document.getElementById('root');
-ReactDOM.createRoot(rootElement); // Se crea el root
-
-// Forma corta
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // Renderizar en la pagina
-root.render(<h1>Hola mundo desde React!</h1>);
+// Para interpretar una funcion dentro de HTML
+root.render(
+  <> {/*Se puede omitir el poner esta etiqueta contenedora con fragment el cual solo es quitar el contenido de las etiquetas*/}
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+  </>
+);
 
+// Componentes
+function Greeting() {
+  function add(x, y) {
+    return x + y;
+  }
+  let user = {
+    firstName: "Oscar",
+    lastname: "Bucio",
+  };
 
-// Componentes en React
+  return <h1>{user.firstName}</h1>
+}
+// Para interpretar código es con llaves
+// JSX es una combinacion de JS con HTML
+// Permite dentro de JS retornar porciones de HTML y se convertirá en js puro
